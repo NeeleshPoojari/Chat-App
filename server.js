1bea5cd571e61fd98e2e54f9accf0557ce07a7bd
Neelesh Poojari <npoojari@sapient.com>
2019-02-19 19:17:02 +0530

    modified functionality with promise

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 var mongoose = require('mongoose')
+mongoose.Promise = Promise
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
@@ -11,7 +12,7 @@ app.use(bodyParser.urlencoded({
     extended: false
 }))
 
-var dbUrl = '**'
+var dbUrl = "**"
 
 var Message = mongoose.model('Message',{
     name: String,
@@ -25,20 +26,41 @@ app.get('/messages', (req, res) => {
     })
 })
 
-app.post('/messages', (req, res) => {
-    var message = new Message(req.body)
-    message.save((err) => {
-        if(err)
-           res.sendStatus(500)
+// app.post('/messages', (req, res) => {
+//     var message = new Message(req.body)
+//     message.save((err) => {
+//         if(err)
+//            res.sendStatus(500)
 
-        io.emit('message', req.body)
-        //messages.push(req.body)
-        res.sendStatus(200);
+//         io.emit('message', req.body)
+//         res.sendStatus(200);
 
-    })
+//     })
    
 
+// })
+
+app.post('/messages', (req, res) => {
+    var message = new Message(req.body)
+    message.save()
+    .then(() => {
+        console.log('Saved')
+        return Message.findOne({message: 'badword'})
+    })
+    .then((censored) => {
+      if(censored) {
+          console.log('Censored message found',censored)
+          return Message.deleteOne({_id: censored.id})
+      }
+     io.emit('message', req.body)
+     res.sendStatus(200);
+    })
+    .catch((err) => {
+        res.send(500)
+        return console.error(err)
+    })
 })
+
 io.on('connection', (socket) => {
     console.log('a user connected')
 })
